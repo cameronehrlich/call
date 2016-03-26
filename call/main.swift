2fc039a1d014c74e816c3f1a2fc0914c5ebe6cc8
cameronehrlich <cameronehrlich@gmail.com>
2016-03-26 06:14:38 -0700

    initial commit

--- call/main.swift
+++ call/main.swift
@@ -7,6 +7,48 @@
 //
 
 import Foundation
+import Darwin
 
-print("Hello, World!")
+extension String {
+    
+    var length : Int {
+        return self.characters.count
+    }
+    
+    func digitsOnly() -> String{
+        let stringArray = self.componentsSeparatedByCharactersInSet(
+            NSCharacterSet.decimalDigitCharacterSet().invertedSet)
+        let newString = stringArray.joinWithSeparator("")
+        
+        return newString
+    }
+    
+}
+
+var numberToCall :String? = ""
+
+for argument in Process.arguments {
+    numberToCall = numberToCall! + argument
+}
+
+numberToCall = numberToCall!.digitsOnly()
+
+if numberToCall?.length == 0 {
+    print("Invalid number")
+    exit(0)
+}
+
+let myAppleScript = "open location \"tel://\(numberToCall!)?audio=yes\""
+
+var error: NSDictionary?
+if let scriptObject = NSAppleScript(source: myAppleScript) {
+    if let output: NSAppleEventDescriptor = scriptObject.executeAndReturnError(&error) {
+        print("Calling \(numberToCall!)...")
+    }
+    else if (error != nil) {
+        print("error: \(error)")
+    }
+}
+
+exit(0)
 
